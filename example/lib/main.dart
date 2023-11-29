0686f196c1d9ca17099fa86433c216046575c6fc
InnovativeDev786 <innovativedev8627@gmail.com>
2023-11-29 23:47:49 +0500

    added optional trailing icon for changing icon in the end of drop down

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -65,15 +65,13 @@ class _MyHomePageState extends State<MyHomePage> {
                   dropdownDecoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(10)),
                       color: Colors.white,
-                      border:
-                          Border.all(color: Colors.grey.shade300, width: 1)),
+                      border: Border.all(color: Colors.grey.shade300, width: 1)),
 
                   ///Disabled Dropdown box decoration to style your dropdown selector [OPTIONAL PARAMETER]  (USE with disabled dropdownDecoration)
                   disabledDropdownDecoration: BoxDecoration(
                       borderRadius: BorderRadius.all(Radius.circular(10)),
                       color: Colors.grey.shade300,
-                      border:
-                          Border.all(color: Colors.grey.shade300, width: 1)),
+                      border: Border.all(color: Colors.grey.shade300, width: 1)),
 
                   ///placeholders for dropdown search field
                   countrySearchPlaceholder: "Country",
@@ -89,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ///defaultCountry: CscCountry.India,
 
                   ///Country Filter [OPTIONAL PARAMETER]
-                  countryFilter: [CscCountry.India,CscCountry.United_States,CscCountry.Canada],
+                  countryFilter: [CscCountry.India, CscCountry.United_States, CscCountry.Canada],
 
                   ///Disable country dropdown (Note: use it with default country)
                   //disableCountry: true,
@@ -101,10 +99,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
 
                   ///DropdownDialog Heading style [OPTIONAL PARAMETER]
-                  dropdownHeadingStyle: TextStyle(
-                      color: Colors.black,
-                      fontSize: 17,
-                      fontWeight: FontWeight.bold),
+                  dropdownHeadingStyle: TextStyle(color: Colors.black, fontSize: 17, fontWeight: FontWeight.bold),
 
                   ///DropdownDialog Item style [OPTIONAL PARAMETER]
                   dropdownItemStyle: TextStyle(
@@ -122,7 +117,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   onCountryChanged: (value) {
                     setState(() {
                       ///store value in country variable
-                      countryValue = value;
+                      if (value != null) {
+                        countryValue = value;
+                      }
                     });
                   },
 
@@ -130,7 +127,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   onStateChanged: (value) {
                     setState(() {
                       ///store value in state variable
-                      stateValue = value;
+                      if (value != null) {
+                        stateValue = value;
+                      }
                     });
                   },
 
@@ -138,7 +137,9 @@ class _MyHomePageState extends State<MyHomePage> {
                   onCityChanged: (value) {
                     setState(() {
                       ///store value in city variable
-                      cityValue = value;
+                      if (value != null) {
+                        cityValue = value;
+                      }
                     });
                   },
 
