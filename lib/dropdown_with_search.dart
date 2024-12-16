42185e3fe857a30a6fac4adbe8d9a433c961bf97
Moulaye Keita <moulaye.c.keita@gmail.com>
2024-12-16 09:25:28 -0800

    Fix error: A value of type 'DialogThemeData' can't be assigned to a variable of type 'DialogTheme'

--- lib/dropdown_with_search.dart
+++ lib/dropdown_with_search.dart
@@ -317,7 +317,7 @@ class CustomDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final DialogTheme dialogTheme = DialogTheme.of(context);
+    final DialogThemeData dialogTheme = DialogTheme.of(context);
     return AnimatedPadding(
       padding: MediaQuery.of(context).viewInsets +
           const EdgeInsets.symmetric(horizontal: 22.0, vertical: 24.0),
