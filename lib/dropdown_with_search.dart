6cc8dc623ffe86f32d3940ca3f4326fb758ca539
Rikaz Zarook <62603541+mzrikaz@users.noreply.github.com>
2024-12-19 11:56:07 +0530

    DialogThemeData set to Dialog Theme Error Fix

--- lib/dropdown_with_search.dart
+++ lib/dropdown_with_search.dart
@@ -317,7 +317,7 @@ class CustomDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final DialogTheme dialogTheme = DialogTheme.of(context);
+    final DialogTheme dialogTheme = DialogTheme();
     return AnimatedPadding(
       padding: MediaQuery.of(context).viewInsets +
           const EdgeInsets.symmetric(horizontal: 22.0, vertical: 24.0),
